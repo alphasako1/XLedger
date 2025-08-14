110a6f57aaac584250a99214f38fa5f8542f79a3
husseingtre <101123313+husseingtre@users.noreply.github.com>
2025-08-15 03:48:06 +0400

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -9,7 +9,6 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# ADD THIS
 def get_db():
     db = SessionLocal()
     try:
